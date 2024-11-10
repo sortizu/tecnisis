ae8e22b3da179299069685a30f5a9f5d21fd5fcd
Garyn14 <garyn.fernando.hr@gmail.com>
2024-11-10 01:30:11 -0500

    init backend

---
+++ backend/art-galery/src/test/java/com/unmsm/movil/tecnisis/art_galery/ArtGaleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmsm.movil.tecnisis.art_galery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGaleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
