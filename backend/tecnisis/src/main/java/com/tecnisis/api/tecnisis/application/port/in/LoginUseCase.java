11b3c5123d3ec32a159f544330478da189e60c04
Sebastian Ortiz Urbai <sortizurbai@gmail.com>
2024-11-20 18:28:49 -0500

    Delete backend directory

--- backend/tecnisis/src/main/java/com/tecnisis/api/tecnisis/application/port/in/LoginUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.tecnisis.api.tecnisis.application.port.in;
-
-import com.tecnisis.api.tecnisis.domain.model.AppUser;
-
-public interface LoginUseCase {
-    AppUser login(String email, String password);
-}
