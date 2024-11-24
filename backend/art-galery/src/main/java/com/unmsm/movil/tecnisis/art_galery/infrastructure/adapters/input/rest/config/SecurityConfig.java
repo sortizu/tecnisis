1a23204aa1062d3d8c08c6d06ddc7ef6b1c87dc2
Manuel Angeles <angelesbocangelmanuel@gmail.com>
2024-11-23 19:39:36 -0500

    authentication is removed from endpoints

--- backend/art-galery/src/main/java/com/unmsm/movil/tecnisis/art_galery/infrastructure/adapters/input/rest/config/SecurityConfig.java
+++ backend/art-galery/src/main/java/com/unmsm/movil/tecnisis/art_galery/infrastructure/adapters/input/rest/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+/*
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,6 +19,26 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/login", "/api/auth/register").permitAll() // Permite acceso público a login y registro
                         .anyRequest().authenticated() // Requiere autenticación para cualquier otra ruta
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+*/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // Deshabilitar CSRF
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Permitir acceso a todos los endpoints
                 );
         return http.build();
     }
